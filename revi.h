94decadae519020878c01d60043d8387b956e87c
Constantine Tarasenkov <iamvfx@gmail.com>
2021-04-24 11:57:27 +0300

    Add explicit commands

--- revi.h
+++ revi.h
@@ -5,6 +5,7 @@ void   reviCreateBuffer      (void ** outBufferID,  unsigned constantFloat4sCoun
 void   reviCreateMesh        (void ** outMeshID,    unsigned vertexFloat4sCount, unsigned colorFloat4sCount, unsigned uvFloat2sCount);
 void   reviCreateTexture     (void ** outTextureID, unsigned pixelUnsignedChar4sWidthxHeight, unsigned levelsCount);
 void   reviCreateRender      (void ** outRenderID,  unsigned pixelUnsignedChar4sWidthxHeight);
+void   reviCreateCommands    (void ** outCommandsID);
 void   reviCreateSignal      (void ** outSignalID);
 
 void   reviSetBuffer         (const void * bufferID,  const void * allConstantFloat4s,                                                        const void * optionalSignalID);
@@ -16,9 +17,11 @@ void   reviGetRender         (const void * renderID,  void * outAllPixelUnsigned
 int    reviGetSignalValue    (const void * signalID);
 void   reviSetSignalValueTo0 (const void * signalID);
 
-void   reviRenderBegin       (const void * renderID, const void * bufferID);
-void   reviRenderDraw        (const void * renderID, const void * programID, const void * meshID, const void * textureID);
-void   reviRenderEnd         (const void * renderID, const void * optionalSignalID);
+void   reviCommandsBegin     (const void * commandsID, const void * renderID, const void * bufferID);
+void   reviCommandsDraw      (const void * commandsID, const void * programID, const void * meshID, const void * textureID);
+void   reviCommandsEnd       (const void * commandsID);
+
+void   reviCommandsExecute   (const void * commandsID, const void * optionalSignalID);
 
 void * reviRenderPresent     (const void * renderID, unsigned renderStartPixelX, unsigned renderStartPixelY, unsigned windowWidth, unsigned windowHeight);
 
@@ -27,6 +30,7 @@ void   reviDestroyBuffer     (const void * bufferID);
 void   reviDestroyMesh       (const void * meshID);
 void   reviDestroyTexture    (const void * textureID);
 void   reviDestroyRender     (const void * renderID);
+void   reviDestroyCommands   (const void * commandsID);
 void   reviDestroySignal     (const void * signalID);
 
 void   reviDestroyEverything (void);
